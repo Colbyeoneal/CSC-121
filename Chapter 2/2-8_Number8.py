ac0b4880f2549add4c187554f1791dfbecaa4ec2
Colbyeoneal <colbyeoneal@students.abtech.edu>
2026-01-26 22:02:06 +0000

    renamed:    hello_world.py -> Chapter 1/hello_world.py 	new file:   Chapter 2/2-8_Number8.py 	renamed:    2.4_Name_cases.py -> Chapter 2/2.4_Name_cases.py

---
+++ Chapter 2/2-8_Number8.py
@@ -0,0 +1,11 @@
+#2-8 Number 8
+#python math practice where all math operations will equal 8
+
+#addition
+print (2 + 6)
+#subtraction
+print (1008 - 1000)
+#multiplication
+print (4 * 2)
+#division
+print (80 / 10)
