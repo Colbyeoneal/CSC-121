2d6c8e32fe72901088a960373b778a7bf40907af
Colbyeoneal <colbyeoneal@students.abtech.edu>
2026-02-02 17:31:36 +0000

    new file:   Chapter 3/3.3 Your_Own_list.py

---
+++ Chapter 3/3.3 Your_Own_list.py
@@ -0,0 +1,5 @@
+favorite_cars = ['Ferrari SF90 Stradale' , '1971 427 Hemi-cuda' , 'Acura NSX Frost']
+print (favorite_cars)
+print (f"If I had a spare 2 million dollars, I would buy a {favorite_cars[0].title()}.")
+print (f"My dream car to restore, and make as close to factory is a {favorite_cars[1].title()}.")
+print (f"As far as racecars go, my favorite is the  {favorite_cars[2].title()}.")
