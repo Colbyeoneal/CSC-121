b861468b18875674c96725b44d44374cc84544b5
Colbyeoneal <colbyeoneal@students.abtech.edu>
2026-02-16 21:23:53 +0000

    new file:   Chapter 4/4-2_Animals.py

---
+++ Chapter 4/4-2_Animals.py
@@ -0,0 +1,4 @@
+birds = ['Falcons' , 'Herons' , 'Chickens']
+for bird in birds:
+     print (f"{bird} are birds, feathers and beaks included")
+print("While they all fly, some have a grotequse sense of speed.")
