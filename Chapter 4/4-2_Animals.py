ba9c6a064c200c6f556b7f930a4a80d53390ad2d
Colbyeoneal <colbyeoneal@students.abtech.edu>
2026-02-17 01:21:57 +0000

    modified:   .vscode/settings.json 	modified:   Chapter 4/4-2_Animals.py 	modified:   Chapter 4/4.10_Slices.py 	new file:   Chapter 4/4.3_Count_to_20.py

--- Chapter 4/4-2_Animals.py
+++ Chapter 4/4-2_Animals.py
@@ -1,4 +1,4 @@
-birds = ['Falcons' , 'Herons' , 'Chickens']
+birds = ['Falcons', 'Herons', 'Chickens']
 for bird in birds:
-     print (f"{bird} are birds, feathers and beaks included")
-print("While they all fly, some have a grotequse sense of speed.")
+    print(f"{bird} are birds, feathers and beaks included")
+print("While they all fly, one has a need for speed.")
