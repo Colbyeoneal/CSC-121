b13588b4cfdad87cb580e4e15e29e0ab3f5e3560
Colbyeoneal <colbyeoneal@students.abtech.edu>
2026-02-23 16:57:16 +0000

    new file:   Chapter 5/5-3_Alien_Colors.py

---
+++ Chapter 5/5-3_Alien_Colors.py
@@ -0,0 +1,9 @@
+alien_color = "blue"
+if alien_color == "green":
+    print ("Small hit: Thats 5 points.")
+elif alien_color == "yellow":
+    print ("Nice shot: Thats 10 points")
+elif alien_color == "red":
+    print ("Critical hit: Thats 15 points!")
+else:
+    print("Missed")
