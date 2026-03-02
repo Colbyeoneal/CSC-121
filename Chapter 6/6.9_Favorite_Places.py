90ab4400568a5a0601daba538e55121982076ed3
Colbyeoneal <colbyeoneal@students.abtech.edu>
2026-03-02 22:50:50 +0000

    modified:   Chapter 6/6.9_Favorite_Places.py

--- Chapter 6/6.9_Favorite_Places.py
+++ Chapter 6/6.9_Favorite_Places.py
@@ -1,5 +1,7 @@
 favorite_places = {
     "Kevin" : ["London", "Greenville", "Charleston"],
     "Aaron" : ["Outer Banks", "Las Vegas", "Austin" ],
-    "Kelly" : ["Miami, New York, Atlanta"]
+    "Kelly" : ["Miami", "New York", "Atlanta"]
 }
+for person, places in favorite_places.items():
+    print(f"{person}'s favorite places are: {', '.join(places)}")
